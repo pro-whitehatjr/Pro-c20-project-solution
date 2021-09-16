37d5de58f6ed67fcb772da4af4e37b78f6e87abe
PRO-Coding <80043809+pro-whitehatjr@users.noreply.github.com>
2021-09-16 13:28:15 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,14 +4,8 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 
-var particle1, particle2,particle3,particle4,particle5;
-var particle6, particle7,particle8,particle9,particle10;
-var block1, block2, block3;
-var block1, block2;
+var block1,block2,block3;
 
-var angle1=60;
-var angle2=50;
-var angle3=60;
 
 function setup(){
     var canvas = createCanvas(550,600);
@@ -23,33 +17,38 @@ function setup(){
       isStatic: true
     }
 
-    plane = Bodies.rectangle(600,height,1200,20,plane_options);
+    plane = Bodies.rectangle(600,580,1200,2,plane_options);
     World.add(world,plane);
   
-    //created multiple of particle bodies 
-    var particle_options = {
+    //created multiple of block bodies 
+    var block1_options = {
       restitution:0.5,
-      friction:0.02
+      friction:0.02,
+      frictionAir:0
     }
 
-    particle1 = Bodies.circle(220,10,10,particle_options);
-    World.add(world,particle1);
-    
-    
-    //created all the three block bodies
-    var block_options={
-      isStatic:true
-    };
+    var block2_options = {
+      restitution:0.7,
+      friction:0.01,
+      frictionAir:0.1
+    }
 
-    block1 = Bodies.rectangle(200,150,150,20,block_options);
+    var block3_options = {
+      restitution:0.01,
+      friction:1,
+      frictionAir:0.3
+    }
+
+    block1 = Bodies.circle(220,10,10,block1_options);
     World.add(world,block1);
-
-    block2 = Bodies.rectangle(300 ,300,150,20,block_options);
+    
+    block2 = Bodies.rectangle(110,50,10,10,block2_options);
     World.add(world,block2);
-
-    block3 = Bodies.rectangle(100,400,150,20,block_options);
+    
+    block3 = Bodies.rectangle(350,50,10,10,block3_options);
     World.add(world,block3);
-
+    
+    
     //styling the bodies here
     fill("brown");
     rectMode(CENTER);
@@ -62,35 +61,13 @@ function draw(){
     Engine.update(engine);
 
   //created shape for plane and stand
-  rect(plane.position.x,plane.position.y,1200,20);
+  rect(plane.position.x,plane.position.y,1200);
   
 
   //created shape for all the paticles
-  ellipse(particle1.position.x,particle1.position.y,10);
- 
+  ellipse(block1.position.x,block1.position.y,30);
+  rect(block2.position.x,block2.position.y,50,50)
+  rect(block3.position.x,block3.position.y,100,50)
 
-  //created shape for all the blocks
-  Matter.Body.rotate(block1,angle1)
-  push();
-  translate(block1.position.x,block1.position.y);
-  rotate(angle1);
-  rect(0,0,150,20);
-  pop();
   
-
-  Matter.Body.rotate(block2,angle2)
-  push();
-  translate(block2.position.x,block2.position.y);
-  rotate(angle2);
-  rect(0,0,150,20);
-  pop();
-    
-
-  Matter.Body.rotate(block3,angle3)
-  push();
-  translate(block3.position.x,block3.position.y);
-  rotate(angle3);
-  rect(0,0,150,20);
-  pop();
-    
 }
